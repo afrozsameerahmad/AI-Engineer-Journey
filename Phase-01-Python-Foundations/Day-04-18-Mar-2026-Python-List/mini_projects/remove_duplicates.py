1b3c96d3ae35ab767c7ea17ec2f97a1cbc5bb99e
Sameer Ahmad <sameerahmad723898@gmail.com>
2026-03-19 02:35:09 +0530

    Day 4: add remove_duplicates.py

---
+++ Phase-01-Python-Foundations/Day-04-18-Mar-2026-Python-List/mini_projects/remove_duplicates.py
@@ -0,0 +1,6 @@
+# Remove Duplicates from List
+# Problem: Write a Python program to remove duplicates from a list.
+
+numbers = [1, 2, 2, 3, 4, 4, 5]
+unique_numbers = list(set(numbers))
+print(f"List after removing duplicates: {unique_numbers}")
