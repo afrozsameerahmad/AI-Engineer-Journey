0d17835902eaef568e06260c09fe2467d1a6b6ec
Sameer Ahmad <sameerahmad723898@gmail.com>
2026-03-19 02:35:09 +0530

    Day 4: add list_search.py

---
+++ Phase-01-Python-Foundations/Day-04-18-Mar-2026-Python-List/mini_projects/list_search.py
@@ -0,0 +1,9 @@
+# List Element Search
+# Problem: Write a Python program to check if a given element exists in a list.
+
+numbers = [10, 20, 30, 40, 50]
+element = 30
+if element in numbers:
+    print(f"{element} exists in the list.")
+else:
+    print(f"{element} does not exist in the list.")
