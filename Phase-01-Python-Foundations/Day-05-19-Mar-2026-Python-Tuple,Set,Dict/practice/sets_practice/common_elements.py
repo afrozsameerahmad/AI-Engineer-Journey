b7ca6b389c06250465bc85a8bf3a803fb7422c40
Sameer Ahmad <sameerahmad723898@gmail.com>
2026-03-20 03:41:28 +0530

    Day 5: add common_elements.py

---
+++ Phase-01-Python-Foundations/Day-05-19-Mar-2026-Python-Tuple,Set,Dict/practice/sets_practice/common_elements.py
@@ -0,0 +1,11 @@
+﻿# Find common elements
+list_1 = [1, 2, 3, 4, 5]
+list_2 = [3, 4, 5, 6]
+list_3 = [4, 5, 7, 8]
+
+s1, s2, s3 = set(list_1), set(list_2), set(list_3)
+print("Common in all:", s1 & s2 & s3)
+print("Common in first two:", s1 & s2)
+
+at_least_two = (s1 & s2) | (s2 & s3) | (s1 & s3)
+print("Present in at least two lists:", at_least_two)
