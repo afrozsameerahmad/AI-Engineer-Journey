8346f75dca1d122ed69df252590c148e21048ae8
Sameer Ahmad <sameerahmad723898@gmail.com>
2026-03-20 03:41:27 +0530

    Day 5: add dict_iteration.py

---
+++ Phase-01-Python-Foundations/Day-05-19-Mar-2026-Python-Tuple,Set,Dict/practice/dictionary/dict_iteration.py
@@ -0,0 +1,14 @@
+﻿# Iterate keys, values, and items
+person = {"name": "Samee", "age": 21, "city": "Delhi"}
+
+print("Keys:")
+for key in person:
+    print(key)
+
+print("\nValues:")
+for value in person.values():
+    print(value)
+
+print("\nItems:")
+for key, value in person.items():
+    print(f"{key}: {value}")
