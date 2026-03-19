d6785c402ec7da485d75dd6d58cbc8d6270ad868
Sameer Ahmad <sameerahmad723898@gmail.com>
2026-03-20 03:41:31 +0530

    Day 5: add tuple_vs_list.py

---
+++ Phase-01-Python-Foundations/Day-05-19-Mar-2026-Python-Tuple,Set,Dict/practice/tuples_practice/tuple_vs_list.py
@@ -0,0 +1,19 @@
+﻿# Quick tuple vs list comparison
+import sys
+
+my_list = [1, 2, 3]
+my_tuple = (1, 2, 3)
+
+print("List size:", sys.getsizeof(my_list))
+print("Tuple size:", sys.getsizeof(my_tuple))
+
+my_list[0] = 99
+print("List after change:", my_list)
+
+try:
+    my_tuple[0] = 99
+except TypeError as err:
+    print("Tuple change error:", err)
+
+points = {(1, 2): "Point A"}
+print("Tuple as dictionary key:", points)
