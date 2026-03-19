c42bb35d71b7237f2ff66e43d4cde0ae2d588292
Sameer Ahmad <sameerahmad723898@gmail.com>
2026-03-20 03:41:29 +0530

    Day 5: add set_comprehension.py

---
+++ Phase-01-Python-Foundations/Day-05-19-Mar-2026-Python-Tuple,Set,Dict/practice/sets_practice/set_comprehension.py
@@ -0,0 +1,13 @@
+﻿# Set comprehension examples
+squares = {x * x for x in range(1, 6)}
+print("Squares:", squares)
+
+even_numbers = {x for x in range(1, 11) if x % 2 == 0}
+print("Even numbers:", even_numbers)
+
+word = "programming"
+unique_letters = {ch for ch in word}
+print("Unique letters:", unique_letters)
+
+common = {x for x in [1, 2, 3, 4] if x in [3, 4, 5, 6]}
+print("Common values:", common)
